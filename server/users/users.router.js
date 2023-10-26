c6319f1fd30a250e47500e21045f25e4f0db8bb5
SandraKoch <sandraannakoch@gmail.com>
2023-10-26 23:01:43 +0200

    user auth middleware and users router outline

--- server/users/users.router.js
+++ server/users/users.router.js
@@ -1,53 +1,108 @@
 const express = require("express");
-const { userValidation } = require("./user.validator");
-const { User } = require("./user.model");
+const { invalidateToken } = require("../auth/auth.middleware");
 const router = express.Router();
 
-router.post(
-  "/signup",
-  // (req, res, next) => userValidation(req, res, next),
-  async (req, res, next) => {
-    try {
-      const { email, password } = req.body;
-      console.log("email:", email, "password:", password);
+router.post("/signup", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    console.log("email:", email, "password:", password);
 
-      // const user = await User.findOne({ email });
-
-      // if (!user) {
-      // const salt = await bcrypt.genSalt(10);
-      // const hash = await bcrypt.hash(password, salt);
-      // const newUser = new User({
-      //   email,
-      //   password: hash,
-      // });
-      // await newUser.save();
-
-      res.status(201).json({
-        status: "Success",
-        code: 201,
-        user: {
-          email,
-        },
-      });
-    } catch (error) {
-      // else {
-      //   res.status(409).json({
-      //     status: "Conflict",
-      //     code: 409,
-      //     message: "Email in use",
-      //   });
-      // }
-      // }
-      console.error(error.message);
-      res.status(500).json({ status: "Internal Server Error", code: 500 });
-    }
+    res.status(201).json({
+      status: "Success",
+      code: 201,
+      user: {
+        email,
+      },
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ status: "Internal Server Error", code: 500 });
   }
-);
+});
 
 router.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  console.log("email:", email, "password:", password);
-  res.send("<h2>Login page comes here</h2>");
+  try {
+    const { email, password } = req.body;
+    console.log("Login", "email:", email, "password:", password);
+    //token just for testing
+    const token = "123";
+    res.status(200).json({
+      status: "Success",
+      code: 200,
+      message: "Successful login!",
+      token,
+      user: { email, token },
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ status: "Internal Server Error", code: 500 });
+  }
+});
+
+router.post("/logout", (req, res) => {
+  try {
+    invalidateToken(req.token);
+    res.status(200).json({
+      message: "ok",
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ status: "Internal Server Error", code: 500 });
+  }
+});
+
+router.get("/current", async (req, res) => {
+  try {
+    //test variables:
+    //const currentUserData = undefined;
+    //const currentUserData = "test@mail.com"
+    if (!currentUserData) {
+      return res.status(401).json({
+        status: "Unauthorized",
+        code: 401,
+        message: "No data here",
+      });
+    } else {
+      res.status(200).json({
+        status: "OK",
+        code: 200,
+        currentUserData,
+      });
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ status: "Internal Server Error", code: 500 });
+  }
+});
+
+router.patch("/update", async (req, res) => {
+  try {
+    //test variables:
+    // const user = undefined;
+    const user = { email: "test@mail.com" };
+    console.log("log1", user);
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: no user authentication" });
+    }
+    try {
+      const { email } = req.body;
+      const updatedUser = { email: email };
+      console.log("log2", updatedUser);
+      return res.status(200).send({
+        status: "OK",
+        code: 200,
+        updatedUser,
+      });
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ status: "Internal Server Error", code: 500 });
+  }
 });
 
 module.exports = router;
