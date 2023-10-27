bcb6c5b5c72f422950679137046891da2571fecf
VeranikaBabik <veranikababik@gmail.com>
2023-10-27 13:36:40 +0200

    fixed userRouter

--- server/app.js
+++ server/app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const { userRouter } = require("./users/users.router");
+const userRouter = require("./users/users.router");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", userRouter);
 
-module.exports = {
-  app,
-};
+module.exports = { app };
