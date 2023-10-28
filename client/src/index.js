d044bf6f9cdb828b36738fa68bdfebb3473056cc
VeranikaBabik <veranikababik@gmail.com>
2023-10-28 18:21:39 +0200

    ShoppingList Component only structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { StrictMode } from "react";
+import "modern-normalize/modern-normalize.css";
+import { BrowserRouter } from "react-router-dom";
+// import { Provider } from "react-redux";
+// import { PersistGate } from "redux-persist/integration/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
