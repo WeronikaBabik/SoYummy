6c83bf697923b5777b49f83487dfea1411424ece
AlinaBielska <alinkabelka5@gmail.com>
2023-10-29 19:32:42 +0100

    installed react-redux and react-router-dom

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Provider>
+        <PersistGate>
+          <App />
+        </PersistGate>
+      </Provider>
+    </HashRouter>
   </React.StrictMode>
 );
 
