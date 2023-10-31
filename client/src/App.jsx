042200fbc4c67007584cc847e9adbff08f79d256
Weronika Babik <weronika.babik@outlook.com>
2023-10-31 18:42:26 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,23 @@ import { useDispatch } from "react-redux";
 import { useAuth } from "./hooks/useAuth";
 import { refreshToken } from "./redux/auth/authSlice";
 import { useEffect } from "react";
-import Home from "./pages/Home";
+import { lazy } from "react";
 
-export const App = () => {
+import SharedLayout from "./components/SharedLayout/SharedLayout";
+// import Loader from './components/Loader/Loader';
+import NotFound from "./components/NotFound/NotFound";
+
+import { Categories } from "./pages/Categories/Categories";
+
+const Main = lazy(() => import("./pages/Main/Main"));
+// const Categories = lazy(() => import('...'));
+// const AddRecipe = lazy(() => import('...'));
+// const MyRecipes = lazy(() => import('...'));
+// const Favorite = lazy(() => import('...'));
+// const ShoppingList = lazy(() => import('...'));
+// const Search = lazy(() => import('...'));
+
+function App() {
   const dispatch = useDispatch();
   const { isRefreshing } = useAuth();
 
@@ -19,10 +33,19 @@ export const App = () => {
     <div>Refreshing user...</div>
   ) : (
     <Routes>
-      {/* <Route path="/" element={<CommonLayout />}> */}
-      <Route index element={<Home />} />
-      <Route path="/shopping-list" element={<ShoppingList />}></Route>
-      {/* </Route> */}
+      <Route path="/" element={<SharedLayout />} />
+      <Route index element={<Main />} />
+      {/* <Route path="/categories/:categoryName" element={<Categories />} />
+        <Route path="/add" element={<AddRecipe />} />
+        <Route path="/my" element={<MyRecipes />} />
+        <Route path="/favorite" element={<Favorite />} />
+        <Route path="/shopping-list" element={<ShoppingList />} />
+        <Route path="/search" element={<Search />} /> */}
+      <Route path="*" element={<NotFound />} />
+      <Route path="/shopping-list" element={<ShoppingList />}></Route>/
+      <Route path="/categories/:categoryName" element={<Categories />} />
     </Routes>
   );
-};
+}
+
+export default App;
