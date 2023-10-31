c949845ad33b6618a94ca1f28e115eb1ac5af550
AlinaBielska <alinkabelka5@gmail.com>
2023-10-31 16:42:00 +0100

    changed modal menu

--- client/src/components/ModalMenu/ModalMenu.jsx
+++ client/src/components/ModalMenu/ModalMenu.jsx
@@ -13,39 +13,37 @@ const ModalMenu = () => {
     setModalOpen(false);
   };
     
-     return (
-         <div>
-             {isModalOpen && (
-                <div>
-                <div>
-                    <svg className={s.logo} width="40" height="40">
-                        <use href="./images/icons/icons.svg#icon-logo"></use>
-                    </svg>
-                    <button autofocus onClick={closeModal}>
-                        <svg className={s.close} width="32" height="32">
-                            <use href="./images/icons/icons.svg#icon-close"></use>
+    return (
+        <div className={s.container}>
+            {isModalOpen && (
+                <div className={s.modal}>
+                    <div className={s.modalButtons}>
+                        <svg className={s.logo} width="40" height="40">
+                            <use href="./images/icons/icons.svg#icon-logo"></use>
                         </svg>
-                    </button>
+                        <button autofocus onClick={closeModal}>
+                            <svg className={s.close} width="32" height="32">
+                                <use href="./images/icons/icons.svg#icon-close"></use>
+                            </svg>
+                        </button>
+                    </div>
+                    <div className={s.modalLinks}>
+                        <Link className={s.link} to="/categories/:categoryName">Categories</Link>
+                        <Link className={s.link} to="/add">Add recipes</Link>
+                        <Link className={s.link} to="/my">My recipes</Link>
+                        <Link className={s.link} to="/favorite">Favorites</Link>
+                        <Link className={s.link} to="/shopping-list">Shopping list</Link>
+                        <Link className={s.link} to="/search">Search</Link>
+                    </div>
                 </div>
-                <Link className={s.link} to="/categories/:categoryName">Categories</Link>
-                <Link className={s.link} to="/add">Add recipes</Link>
-                <Link className={s.link} to="/my">My recipes</Link>
-                <Link className={s.link} to="/favorite">Favorites</Link>
-                <Link className={s.link} to="/shopping-list">Shopping list</Link>
-                <Link className={s.link} to="/search">
-                    <svg className={s.logo} width="40" height="40">
-                        <use href="./images/icons/icons.svg#icon-search"></use>
+            )}
+            {isModalOpen ? undefined : (
+                <button className={s.menu} onClick={openModal}>
+                    <svg className={s.hamburger} width="28" height="28">
+                        <use href="./images/icons/icons.svg#icon-mobile-menu"></use>
                     </svg>
-                    <span>Search</span>
-                </Link>
-            </div>
-             )}
-            
-            <button className={s.menu} onClick={openModal}>
-                <svg className={s.hamburger} width="28" height="28">
-                    <use href="./images/icons/icons.svg#icon-mobile-menu"></use>
-                </svg>
-            </button>
+                </button>
+            )}
         </div>
     );
 };
