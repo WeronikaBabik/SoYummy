10126371d9858a42e420685f40f2a637206f159e
SandraKoch <sandraannakoch@gmail.com>
2023-11-02 16:12:12 +0100

    user logo component

--- client/src/components/UserLogo/UserLogo.jsx
+++ client/src/components/UserLogo/UserLogo.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { logout } from "../../redux/auth/operations";
+import css from "./UserLogo.module.css";
+
 const UserLogo = () => {
-    return (
-        <h3>user</h3>
-    )
+  const dispatch = useDispatch();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleClose = (e) => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = (e) => {
+    dispatch(logout());
+    setAnchorEl(null);
+  };
+
+  //
+  return (
+    <div className={css.box}>
+      {/* <symbol id="icon-user" viewBox="0 0 32 32"> */}
+      <button className={css.userImg}>
+        <svg></svg>
+      </button>
+      {/* <img src="" alt="user avatar" className={css.userImg} /> */}
+      <p className={css.text}>Username</p>
+    </div>
+  );
 };
 
 export default UserLogo;
