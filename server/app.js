3a33115a8f6ad557a43776918602dde298a96ada
BartekJedliczka <bartekjedliczka@gmail.com>
2023-11-05 11:12:43 +0100

    create favorite

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ const usersRouter = require("./routes/api/users");
 const { recipesRouter } = require("./routes/api/recipes");
 const { searchRouter } = require("./routes/api/search");
 const { ingredientsRouter } = require("./routes/api/ingredients");
+const { ownRecipesRouter } = require("./routes/api/ownRecipes");
+const { favoriteRouter } = require("./routes/api/favorite");
 //const { categoryRouter } = require("./routes/api/categorieslist");
 const { shoppingListRouter } = require("./routes/api/shoppingList");
 
@@ -12,6 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
 // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/users", usersRouter);
@@ -19,6 +22,8 @@ app.use("/api/users", usersRouter);
 app.use("/api/recipes", recipesRouter);
 app.use("/api/search", searchRouter);
 app.use("/api/ingredients", ingredientsRouter);
+app.use("/api/ownRecipes", ownRecipesRouter);
+app.use("/api/favorite", favoriteRouter);
 //app.use("/api/category", categoryRouter);
 app.use("/api/shopping-list", shoppingListRouter);
 
