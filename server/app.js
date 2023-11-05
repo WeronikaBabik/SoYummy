418bdf74db9ce6a7ab3e88647338bfa33515ec28
SandraKoch <sandraannakoch@gmail.com>
2023-11-05 20:51:14 +0100

    app.js fix

--- server/app.js
+++ server/app.js
@@ -4,7 +4,10 @@ const usersRouter = require("./routes/api/users");
 const { recipesRouter } = require("./routes/api/recipes");
 const { searchRouter } = require("./routes/api/search");
 const { ingredientsRouter } = require("./routes/api/ingredients");
+const { ownRecipesRouter } = require("./routes/api/ownRecipes");
+const { favoriteRouter } = require("./routes/api/favorite");
 const { categoryRouter } = require("./routes/api/categorieslist");
+const { popularRecipeRouter } = require("./routes/api/popularRecipe");
 const { shoppingListRouter } = require("./routes/api/shoppingList");
 
 const app = express();
@@ -19,7 +22,10 @@ app.use("/api/users", usersRouter);
 app.use("/api/recipes", recipesRouter);
 app.use("/api/search", searchRouter);
 app.use("/api/ingredients", ingredientsRouter);
+app.use("/api/ownRecipes", ownRecipesRouter);
+app.use("/api/favorite", favoriteRouter);
 app.use("/api/category", categoryRouter);
+app.use("/api/popularRecipe", popularRecipeRouter);
 app.use("/api/shopping-list", shoppingListRouter);
 
 module.exports = { app };
