464964799dae75692c3472c1c6b8135cde654db1
SandraKoch <sandraannakoch@gmail.com>
2023-11-06 00:48:08 +0100

    opening modal windows

--- client/src/components/UserLogo/UserLogo.jsx
+++ client/src/components/UserLogo/UserLogo.jsx
@@ -1,57 +1,41 @@
 import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { logout } from "../../redux/auth/operations";
+import { useSelector } from "react-redux";
 import css from "./UserLogo.module.css";
 import { selectUser } from "../../redux/auth/selectors";
-// import { UserInfoModal } from "../UserInfoModal";
-// import { LogoutModal } from "../LogoutModal";
+import { UserInfoModal } from "../UserInfoModal";
+import { LogoutModal } from "../LogoutModal";
 
 const UserLogo = () => {
-  const dispatch = useDispatch();
-  // const [anchorEl, setAnchorEl] = useState(null);
-  // const open = Boolean(anchorEl);
   const user = useSelector(selectUser);
+  const [isOpen, setIsOpen] = useState(false);
 
-  // const handleClick = (e) => {
-  //   setAnchorEl(e.currentTarget);
-  // };
+  const handleLogoClick = (e) => {
+    setIsOpen((current) => !current);
+  };
 
-  // const handleClose = (e) => {
-  //   setAnchorEl(null);
-  // };
+  const [toggleDialog, setToggleDialog] = useState();
+  const [toggleLogoutDialog, setToggleLogoutDialog] = useState();
 
-  // const handleLogout = (e) => {
-  //   dispatch(logout());
-  //   setAnchorEl(null);
-  // };
+  const handleEdit = () => {
+    setIsOpen(false);
+    setToggleDialog((current) => !current);
+  };
 
-  // modal - dialog
-  // const [openDialog, setOpenDialog] = useState(false);
-
-  // const handleEdit = () => {
-  //   setOpenDialog(true);
-  // };
-
-  // const handleCloseDialog = () => {
-  //   setOpenDialog(false);
-  // };
+  const handleLogoutModal = () => {
+    setToggleLogoutDialog((current) => !current);
+  };
 
   return (
     <>
       <div className={css.userLogo}>
-        <button
-          className={css.userImg}
-          // onClick={handleClick}
-        >
+        <button className={css.userImg} onClick={handleLogoClick}>
           <span className={css.iconSpan}>
             <svg
               className={css.iconSvg}
               stroke="currentColor"
               fill="none"
-              stroke-width="2"
+              strokeWidth="2"
               viewBox="0 0 24 24"
-              stroke-linecap="round"
-              stroke-linejoin="round"
               height="24px"
               width="24px"
               xmlns="http://www.w3.org/2000/svg"
@@ -64,19 +48,14 @@ const UserLogo = () => {
         <p className={css.text}>{user.name}</p>
       </div>
 
-      <div className={css.isHidden} data-modal>
-        <div
-          // anchorEl={anchorEl}
-          className={css.userLogoModal}
-          // onClose={handleClose}
-          // open={open}
-        >
+      <div
+        onClick={handleLogoClick}
+        className={isOpen ? css.backdrop : css.isHidden}
+      >
+        <div className={css.userLogoModal}>
           <div className={css.edit}>
             <p>Edit Profile</p>
-            <button
-              className={css.editButton}
-              // onClick={handleEdit}
-            >
+            <button className={css.editButton} onClick={handleEdit}>
               <svg
                 width="14"
                 height="15"
@@ -87,17 +66,14 @@ const UserLogo = () => {
                 <path
                   d="M1.62571 11.5323C1.67166 11.1188 1.69463 10.9121 1.75719 10.7188C1.81269 10.5474 1.89111 10.3842 1.99031 10.2338C2.10213 10.0642 2.24922 9.91713 2.5434 9.62295L9.91636 2.25001C10.5607 1.60568 11.6054 1.60568 12.2497 2.25001V2.25001C12.894 2.89434 12.894 3.93901 12.2497 4.58334L4.87673 11.9563C4.58255 12.2505 4.43546 12.3976 4.26589 12.5094C4.11546 12.6086 3.9523 12.687 3.78085 12.7425C3.58761 12.8051 3.38087 12.828 2.96738 12.874L1.45801 13.0417L1.62571 11.5323Z"
                   stroke="#23262A"
-                  stroke-width="1.3"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1.3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 ></path>
               </svg>
             </button>
           </div>
-          <button
-            className={css.logoutButton}
-            // onClick={handleLogout}
-          >
+          <button className={css.logoutButton} onClick={handleLogoutModal}>
             <span className={css.logoutButtonText}>Logout</span>
             <span>
               <svg
@@ -110,25 +86,24 @@ const UserLogo = () => {
                 <path
                   d="M2.75 9.5H12.75"
                   stroke="#FAFAFA"
-                  stroke-width="1.3"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1.3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 ></path>
                 <path
                   d="M9 4.25L14.25 9.5L9 14.75"
                   stroke="#FAFAFA"
-                  stroke-width="1.3"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1.3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 ></path>
               </svg>
             </span>
           </button>
         </div>
       </div>
-
-      {/* <UserInfoModal open={openDialog} onCLose={handleCloseDialog} /> */}
-      {/* <LogoutModal open={openDialog} onCLose={handleCloseDialog} /> */}
+      {toggleDialog ? <UserInfoModal /> : <></>}
+      {toggleLogoutDialog ? <LogoutModal /> : <></>}
     </>
   );
 };
